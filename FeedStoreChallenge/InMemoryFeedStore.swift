e3785a61eb15c0ce522a4f2afb1a7544946e23d6
GokmenAkar <gokmen.akar92@gmail.com>
2020-09-13 15:30:49 +0300

    Add `InMemoryStore` file

---
+++ FeedStoreChallenge/InMemoryFeedStore.swift
@@ -0,0 +1,9 @@
+//
+//  InMemoryFeedStore.swift
+//  FeedStoreChallenge
+//
+//  Created by Developer on 13.09.2020.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
